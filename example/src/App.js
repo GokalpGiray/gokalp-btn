154e0aef55987e8ca3de7c7d3049b40ea5baf520
Gokalp Giray <gokalpgiray24@gmail.com>
2022-04-21 11:23:48 +0300

    GG-1 Butonlar oluşturuldu

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react'
 
-import { ExampleComponent } from 'gokalp-btn'
+import { ExampleComponent, Gbutton } from 'gokalp-btn'
 import 'gokalp-btn/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <>
+      <ExampleComponent text="Create React Library Example 😄" />
+      <Gbutton type="primary" text="Primary Button"/>
+      <Gbutton type="secondary" text="Secondary Button"/>
+      <Gbutton type="primary-o" text="Primary Outline Button"/>
+      <Gbutton type="secondary-o" text="Secondary Outline Button"/>
+      <Gbutton text="Default Button"/>
+    </>
+  );
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -4,3 +4,33 @@ import styles from './styles.module.css'
 export const ExampleComponent = ({ text }) => {
   return <div className={styles.test}>Example Component: {text}</div>
 }
+
+export const Gbutton = (props) => {
+  let buttonType;
+
+  if (props.type === "primary") {
+    buttonType = styles.primary;
+  } else if (props.type === "primary-o") {
+    buttonType = styles.primaryOutline;
+  } else if (props.type === "secondary") {
+    buttonType = styles.secondary;
+  } else if (props.type === "secondary-o") {
+    buttonType = styles.secondaryOutline;
+  } else if (!props.type) {
+    buttonType = styles.default;
+  }
+
+  return (
+    <button
+      {...props}
+      className={buttonType}
+      style={{
+        borderRadius: "5px",
+        padding: "5px 10px",
+        cursor: "pointer"
+      }}
+    >
+      {props.text}
+    </button>
+  )
+};
